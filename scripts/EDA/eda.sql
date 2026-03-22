96b96d5367a23ff2aab777bbb0821a4889397e4b
Damilola Olatayo <77675650+Boss2021-eng@users.noreply.github.com>
2026-03-22 22:06:02 +0100

    Update eda.sql to include metrics for measures

--- scripts/EDA/eda.sql
+++ scripts/EDA/eda.sql
@@ -35,3 +35,49 @@ SELECT
     MAX(Birth_date) AS oldest_birth_date,
     timestampdiff(year, MAX(Birth_date),current_date) AS oldest_age
 FROM dw_gold.dim_customers;
+
+-- Measures Exploration
+-- 1. Find the Total Sales
+SELECT SUM(sales_amount) FROM dw_gold.facts_sales AS total_sales_amount;
+
+-- 2. Find how many items are sold
+SELECT SUM(quantity) AS total_quantity FROM dw_gold.facts_sales;
+
+-- 3. Find the average selling price
+SELECT AVG(price) AS Average_selling_price FROM dw_gold.facts_sales ;
+
+-- 4. Find the total number of orders
+SELECT COUNT(order_number) AS Total_numbers_of_orders FROM dw_gold.facts_sales;
+SELECT COUNT( DISTINCT order_number) AS Total_numbers_of_orders FROM dw_gold.facts_sales;
+
+-- 5. Find the total numbers of products
+SELECT COUNT(product_key) AS Total_numbers_of_products FROM dw_gold.dim_products;
+SELECT COUNT(DISTINCT product_key) AS Total_numbers_of_products FROM dw_gold.dim_products;
+
+-- 6. Find the total numbers of customers
+SELECT COUNT(customer_key) AS Total_numbers_of_orders FROM dw_gold.dim_customers;
+SELECT COUNT(DISTINCT customer_key) AS Total_numbers_of_customers FROM dw_gold.dim_customers;
+
+-- 7. Find the total numbers of customers that has placed an order; 
+SELECT 
+	count(customer_id) total_customers
+FROM dw_gold.dim_customers
+WHERE EXISTS (
+	SELECT 1
+    FROM dw_gold.facts_sales
+    WHERE dw_gold.dim_customers.customer_key = dw_gold.dim_customers.customer_key
+);
+SELECT COUNT(DISTINCT customer_key) AS total_customers FROM dw_gold.facts_sales;
+
+-- Generate a report reporting all key metrics
+SELECT 'Total Sales' AS measure_name, SUM(sales_amount) AS measure_value FROM dw_gold.facts_sales 
+UNION
+SELECT 'Total Quantity',  SUM(quantity)  FROM dw_gold.facts_sales
+UNION 
+SELECT 'Average Selling Price', AVG(price) FROM dw_gold.facts_sales 
+UNION 
+SELECT 'Total Numbers of Orders' , COUNT( DISTINCT order_number)  FROM dw_gold.facts_sales
+UNION 
+SELECT 'Total Numbers of Products', COUNT(DISTINCT product_key)  FROM dw_gold.dim_products
+UNION
+SELECT 'Total Numbers of Customers', COUNT(DISTINCT customer_key)  FROM dw_gold.dim_customers;
