4e75f118bd5b5f58bb7dd62e36ce023c4f8f6d0e
Damilola Olatayo <77675650+Boss2021-eng@users.noreply.github.com>
2026-03-26 12:14:20 +0100

    Update silver_load.sql to change location of sales

--- scripts/silver/silver_load.sql
+++ scripts/silver/silver_load.sql
@@ -99,7 +99,7 @@ BEGIN
 	-- Data cleaning and insertion for the sales details table
 	TRUNCATE dw_silver.crm_sales_details;
 	INSERT INTO dw_silver.crm_sales_details
-	(sls_ord_num, sls_prd_key, sls_cust_id, sls_order_dt, sls_ship_dt, sls_due_dt, sls_sales, sls_quantity, sls_price)
+	(sls_ord_num, sls_prd_key, sls_cust_id, sls_order_dt, sls_ship_dt, sls_due_dt, sls_quantity, sls_price, sls_sales)
 	SELECT 
 		sls_ord_num,
 		sls_prd_key,
